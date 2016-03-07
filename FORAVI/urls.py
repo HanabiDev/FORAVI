889dfa48b34c455835e7b5f7a58afba1b8f8045b
Juan Gómez <gomexa@hotmail.com>
2016-03-07 11:50:07 -0500

    Client templates updated

--- FORAVI/urls.py
+++ FORAVI/urls.py
@@ -7,6 +7,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin', include('backend.urls')),
     url(r'^redactor/', include('redactor.urls')),
-    url(r'^clientes/', include('clients.urls', 'clients')),
+    url(r'^asociados/', include('clients.urls', 'clients')),
     url(r'^', include('frontend.urls', 'frontend')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
